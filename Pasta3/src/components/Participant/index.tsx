097cdb998b21fc38ceaeeb772a719744dcc93937
Raquel <100868145+raquelm16@users.noreply.github.com>
2023-05-18 18:36:00 -0300

    Add files via upload

---
+++ Pasta3/src/components/Participant/index.tsx
@@ -0,0 +1,26 @@
+import {Image, View, Text,TouchableOpacity, Button} from 'react-native';
+import {styles} from './styles';
+
+type Props = {
+    name: string;
+    onRemove: () => void;
+}
+
+
+export function Participant({name, onRemove} : Props){
+    return(
+        <View style={styles.container}>
+
+       <TouchableOpacity style={styles.radio} onPress={onRemove}>
+            </TouchableOpacity>
+
+            <Text style={styles.name}>
+                {name}
+            </Text>
+
+            <TouchableOpacity style={styles.button} onPress={onRemove}>
+                <Image style={styles.buttonImage} source={require('../../../assets/trash.svg')} />
+            </TouchableOpacity>
+        </View>
+    )
+}
